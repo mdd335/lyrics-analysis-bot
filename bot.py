8a8fdd3545c1efc9aae7aaca3c72f767868fbe39
Malte <maltebro420@gmail.com>
2020-12-30 14:22:48 +0100

    1 Artist, several Keywords, Output as Table, Telegram, multiple user requests

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from analysis_creator import Analysis_creator
+from user_request import User_request
 
 import logging
 from typing import Dict
@@ -15,17 +16,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-artists = []
-keywords = []
-years = []
-
 ARTIST, KEYWORD, YEAR_START, YEAR_END, ANALYSIS = range(5)
+
+# TODO: Needs garbage collection if bot lives for a long time
+request_dictionary = {}
 
 
 def start(update: Update, context: CallbackContext) -> int:
-    artists.clear()
-    keywords.clear()
-    years.clear()
+    new_user_request = User_request(update.message.chat.id)
+    request_dictionary[update.message.chat.id] = new_user_request
     update.message.reply_text("Hi! I am the LyricsBot.")
     update.message.reply_text("If you tell me an artist, a time span and one or more keyword(s), I will tell you the percentage of their songs in each year in the time span that contain the keyword(s).")
     update.message.reply_text("Type /info for more detailed info. Type /start at any time to start from the beginning.")
@@ -38,7 +37,7 @@ def info(update: Update, context: CallbackContext) -> int:
     update.message.reply_text("Info")
     update.message.reply_text("Info")
     update.message.reply_text("Info")
-    if artists == []:
+    if request_dictionary[update.message.chat.id].artist is None:
         update.message.reply_text("Type in an artist to start. Spell the name exactly as it is spelled on genius.com")
         return ARTIST
     else:
@@ -48,7 +47,7 @@ def info(update: Update, context: CallbackContext) -> int:
 
 def artist_chosen(update: Update, context: CallbackContext) -> int:
     artist = update.message.text
-    artists.append(artist)
+    request_dictionary[update.message.chat.id].artist = artist
     if artist == "Money Boy":
         update.message.reply_text(f'Gute Wahl Mois')
     update.message.reply_text(f'I will analyze {artist}.')
@@ -58,11 +57,11 @@ def artist_chosen(update: Update, context: CallbackContext) -> int:
 
 
 def yearstart_chosen(update: Update, context: CallbackContext) -> int:
-    yearstart = update.message.text
-    if yearstart.isnumeric():
-        if len(yearstart) == 4 and 1799 < int(yearstart) < 2031:
-            years.append(int(yearstart))
-            update.message.reply_text(f'I will start with year {yearstart}.')
+    year_start = update.message.text
+    if year_start.isnumeric():
+        if len(year_start) == 4 and 1799 < int(year_start) < 2031:
+            request_dictionary[update.message.chat.id].year_start = year_start
+            update.message.reply_text(f'I will start with year {year_start}.')
             update.message.reply_text(f'Next, please choose the year in which the time span should end.')
             return YEAR_END
     update.message.reply_text(f'Please enter a year between 1900 and 2030.')
@@ -70,21 +69,21 @@ def yearstart_chosen(update: Update, context: CallbackContext) -> int:
 
 
 def yearend_chosen(update: Update, context: CallbackContext) -> int:
-    yearend = update.message.text
-    if yearend.isnumeric():
-        if len(yearend) == 4 and years[0] <= int(yearend) < 2031:
-            years.append(int(yearend))
-            update.message.reply_text(f'I will analyze the years between {str(years[0])} and {yearend}.')
+    year_end = update.message.text
+    if year_end.isnumeric():
+        if len(year_end) == 4 and request_dictionary[update.message.chat.id].year_start <= int(year_end) < 2031:
+            request_dictionary[update.message.chat.id].year_end = year_end
+            update.message.reply_text(f'I will analyze the years between {str(request_dictionary[update.message.chat.id].year_start)} and {year_end}.')
             update.message.reply_text(f'Next, please choose the first keyword to analyze.')
             return KEYWORD
-    update.message.reply_text(f'Please enter a year between {str(years[0])} and 2030.')
+    update.message.reply_text(f'Please enter a year between {str(request_dictionary[update.message.chat.id].year_start)} and 2030.')
     return YEAR_END
 
 
 def keyword_chosen(update: Update, context: CallbackContext) -> int:
     keyword = update.message.text
-    keywords.append(keyword)
-    update.message.reply_text(f'You selected the artist "{artists[0]}", the time span {years[0]} to {years[1]} and keyword(s) {str(keywords)}.')
+    request_dictionary[update.message.chat.id].keywords.append(keyword)
+    update.message.reply_text(f'You selected the artist "{request_dictionary[update.message.chat.id].artist}", the time span {request_dictionary[update.message.chat.id].year_start} to {request_dictionary[update.message.chat.id].year_end} and keyword(s) {str(request_dictionary[update.message.chat.id].keywords)}.')
     update.message.reply_text(f'Add another keyword or type /analyze to start Analysis')
 
     return ANALYSIS
@@ -93,12 +92,13 @@ def keyword_chosen(update: Update, context: CallbackContext) -> int:
 def analysis_started(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(f'Analysis started. This might take a while.')
     my_analysis_creator = Analysis_creator()
-    csv_file, example_string = my_analysis_creator.analyze_artist(artists[0], keywords, range(years[0], (years[1] + 1)))
+    csv_file, example_string = my_analysis_creator.analyze_artist(request_dictionary[update.message.chat.id].artist, request_dictionary[update.message.chat.id].keywords, range(request_dictionary[update.message.chat.id].year_start, (request_dictionary[update.message.chat.id].year_end + 1)))
     update.message.reply_text(f'Here is the data as a csv file:')
     context.bot.send_document(chat_id=update.message.chat_id, document=csv_file)
     update.message.reply_text(example_string)
     update.message.reply_text(f'Thank you. Type /start anytime to start over.')
 
+    # TODO: Remove user request from map
     return ConversationHandler.END
 
 
@@ -111,7 +111,7 @@ def main() -> None:
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    # Add conversation handler with the states ...
     conv_handler = ConversationHandler(
         entry_points=[
             CommandHandler('start', start),
